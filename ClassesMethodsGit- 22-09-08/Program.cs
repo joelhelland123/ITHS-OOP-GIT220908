22269397b39f3679ef0aa21ec64b067254f105d8
joelhelland123 <joel.helland@icloud.com>
2022-09-29 10:08:14 +0200

    Skapade metod för att skriva ut till en textfil

--- ClassesMethodsGit- 22-09-08/Program.cs
+++ ClassesMethodsGit- 22-09-08/Program.cs
@@ -16,6 +16,7 @@ Console.WriteLine(webPage.ClassName);
 Console.WriteLine(webPage.ClassMessages);
 Console.WriteLine(webPage.Courses);
 Console.WriteLine(webPage.BottomTags);
+printTxt();
 //Console.ReadLine();
 FileInfo file = new FileInfo(@"C:\Users\Joel Helland\Documents\ITHS\Test\ITHS-Filhantering2022-09-27\TextWebsiteGenerator.txt");
 
@@ -23,12 +24,14 @@ FileInfo file = new FileInfo(@"C:\Users\Joel Helland\Documents\ITHS\Test\ITHS-Fi
 FileStream fs = file.Open(FileMode.Append, FileAccess.Write, FileShare.Read);
 
 //Create StreamWriter object to write string to FileSream
+public void printTxt() { 
 StreamWriter sw = new StreamWriter(fs);
 sw.WriteLine("<!DOCTYPE>\n<html>\n<head>\n<style>\np{color: blue; }\n</style>\n</head>");
 sw.WriteLine(webPage.ClassName);
 sw.WriteLine(webPage.ClassMessages);
 sw.WriteLine(webPage.Courses);
 sw.WriteLine(webPage.BottomTags);
+    }
 
 // Man ska stänga filen efter användning!
 sw.Close();
